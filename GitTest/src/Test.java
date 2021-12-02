fa3b78589918f92e833e687341f67e8736a94151
smhrd <smhrd@DESKTOP-31C4914>
2021-12-02 15:46:26 +0900

    환경구축 설명추가

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -3,6 +3,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// 1. JDK 버전
+		// 2. Tomcat 버전
+		// 3. Encoding 방식(UTF-8)
 		System.out.println("First Code!");
 		System.out.println("1등은 우리팀");
 	}
